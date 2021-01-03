e723d3ef9cb77a38d175d64b5ba273afcf3d831f
Shariful Alam <32779437+msharifulalam@users.noreply.github.com>
2021-01-03 10:42:25 +0600

    js changed

--- js/main.js
+++ js/main.js
@@ -14,7 +14,19 @@
     }
     init();
 	// Menu Dynamic
-
+	
+	// Show hide sidebar menu
+	// Let this snippet run before your hashchange event binding code
+	if (!window.HashChangeEvent)(function(){
+	  var lastURL = document.URL;
+	  window.addEventListener("hashchange", function(event){
+	    Object.defineProperty(event, "oldURL", {enumerable:true,configurable:true,value:lastURL});
+	    Object.defineProperty(event, "newURL", {enumerable:true,configurable:true,value:document.URL});
+	    lastURL = document.URL;
+	  });
+	  console.log(lastURL);
+	}());
+	
 	$('.mobile_menu').on('click', function(e){
 		e.preventDefault();
 
