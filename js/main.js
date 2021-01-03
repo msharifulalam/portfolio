10c02f92d741cdefb41f4e7e974d4f2aefdfca01
Shariful Alam <32779437+msharifulalam@users.noreply.github.com>
2021-01-03 10:51:57 +0600

    js changed

--- js/main.js
+++ js/main.js
@@ -17,15 +17,10 @@
 	
 	// Show hide sidebar menu
 	// Let this snippet run before your hashchange event binding code
-	if (!window.HashChangeEvent)(function(){
-	  var lastURL = document.URL;
-	  window.addEventListener("hashchange", function(event){
-	    Object.defineProperty(event, "oldURL", {enumerable:true,configurable:true,value:lastURL});
-	    Object.defineProperty(event, "newURL", {enumerable:true,configurable:true,value:document.URL});
-	    lastURL = document.URL;
-	  });
-	  console.log(lastURL);
-	}());
+	
+	window.addEventListener("hashchange", function(event){
+		console.log(event);
+	});
 	
 	$('.mobile_menu').on('click', function(e){
 		e.preventDefault();
