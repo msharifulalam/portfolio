d4cdf6072e6f2be9f4c2f31c1730afac5d7311e3
Shariful Alam <32779437+msharifulalam@users.noreply.github.com>
2021-01-04 19:04:51 +0600

    updated router.js

--- haschange/router.js
+++ haschange/router.js
@@ -1,106 +1,108 @@
 'use strict';
 
-function Router(routes){
+function Router(routes) {
 
-	try{
-		
-		if(!routes){
-			throw 'Error: Routes parameter is mandatory!';
-		}
-		this.constructor(routes);
-		this.init();
+    try {
 
-	}catch(e){
-		console.error(e);
-	}
+        if (!routes) {
+            throw 'Error: Routes parameter is mandatory!';
+        }
+        this.constructor(routes);
+        this.init();
+
+    } catch (e) {
+        console.error(e);
+    }
 
 }
 
 Router.prototype = {
 
-	routes: undefined,
-	rootElem: undefined,
-	// routeSet: undefined,
+    routes: undefined,
+    rootElem: undefined,
+    // routeSet: undefined,
 
-	constructor: function(routes){
-		this.routes = routes;
-		this.rootElem = document.getElementById("pagingbita");
-	},
+    constructor: function(routes) {
+        this.routes = routes;
+        this.rootElem = document.getElementById("pagingbita");
+    },
 
-	init: function(){
+    init: function() {
 
-		let r = this.routes;
-		(function(scope, r){
+        let r = this.routes;
+        (function(scope, r) {
 
-			window.addEventListener('hashchange', function(e){
-				scope.hasChanged(scope, r);
-			});
+            window.addEventListener('hashchange', function(e) {
+                scope.hasChanged(scope, r);
+                if (window.matchMedia("(max-width: 767px)").matches === true) {
+                    $(".sidebar-left-content").toggleClass("hidden-xs visible-xs");
+                }
+            });
 
-		})(this, r);
+        })(this, r);
 
-		this.hasChanged(this, r);
-	
-	},
+        this.hasChanged(this, r);
 
-	hasChanged: function(scope, r){
-		if (window.location.hash.length > 0) {
+    },
 
-			for(var i = 0, len = r.length; i < len; i++){
-				var route = r[i];
+    hasChanged: function(scope, r) {
+        if (window.location.hash.length > 0) {
 
-				if (route.isActiveRoute(window.location.hash.substr(1))) {
-					scope.goToRoute(route.htmllink);
-					
-					let root1 = route;
-					setTimeout(() => root1.func() , 2000);
-				}
-			}
+            for (var i = 0, len = r.length; i < len; i++) {
+                var route = r[i];
 
-		}else{
+                if (route.isActiveRoute(window.location.hash.substr(1))) {
+                    scope.goToRoute(route.htmllink);
 
-			for(var i = 0, len = r.length; i < len; i++){
-				var route = r[i];
+                    let root1 = route;
+                    setTimeout(() => root1.func(), 2000);
+                }
+            }
 
-				if (route.defaultRoute) {
-					scope.goToRoute(route.htmllink);
-					window.location.hash = '#index';
+        } else {
 
-					let root2 = route;
-					setTimeout(() => root2.func , 500);
-				}
-			}
+            for (var i = 0, len = r.length; i < len; i++) {
+                var route = r[i];
 
-		}
+                if (route.defaultRoute) {
+                    scope.goToRoute(route.htmllink);
+                    window.location.hash = '#index';
 
-	},
+                    let root2 = route;
+                    setTimeout(() => root2.func, 500);
+                }
+            }
 
-	// getSingleRoute: function(){
-	// 	return this.routeSet;
-	// },
+        }
 
-	runSingleRoute: function(func){
-		return func;
-	},
+    },
 
-	goToRoute: function(htmllink){
-		
-		(function(scope){
+    // getSingleRoute: function(){
+    // 	return this.routeSet;
+    // },
 
-			let url = 'views/' + htmllink,
-				xmlhr = new XMLHttpRequest();
+    runSingleRoute: function(func) {
+        return func;
+    },
 
-			xmlhr.onreadystatechange = function(){
-				if(this.readyState === 4 && this.status === 200){
-					scope.rootElem.innerHTML = this.responseText;
-				}
-			};
+    goToRoute: function(htmllink) {
 
-			xmlhr.open('GET', url, true);
-			xmlhr.send();
+        (function(scope) {
 
-		})(this);
+            let url = 'views/' + htmllink,
+                xmlhr = new XMLHttpRequest();
 
-	}
+            xmlhr.onreadystatechange = function() {
+                if (this.readyState === 4 && this.status === 200) {
+                    scope.rootElem.innerHTML = this.responseText;
+                }
+            };
+
+            xmlhr.open('GET', url, true);
+            xmlhr.send();
+
+        })(this);
+
+    }
 
 };
-
